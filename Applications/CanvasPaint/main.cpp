5a54fa8a9ae0c6fb486c92971caeb39fd520454b
Michele Dell'Ambrogio <mda@disneyresearch.com>
2016-07-14 16:47:07 +0200

    canvas paint

---
+++ Applications/CanvasPaint/main.cpp
@@ -0,0 +1,27 @@
+
+#include <QGuiApplication>
+#include <QQmlContext>
+#include <QQmlApplicationEngine>
+#include <QQuickStyle>
+
+
+int main(int argc, char *argv[])
+{
+	QGuiApplication::setApplicationName("CanvasPaint");
+	QGuiApplication::setOrganizationName("CanvasPaint");
+	QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+	QQuickStyle::setStyle("Material");
+
+	QGuiApplication app(argc, argv);
+
+	QQmlApplicationEngine engine;
+
+	//AppController appController;
+	//engine.rootContext()->setContextProperty("ctrl", &appController);
+
+	engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+	return app.exec();
+}
+
--- Applications/SvgEditor/main.qml
+++ Applications/SvgEditor/main.qml
@@ -1,4 +1,4 @@
-import QtQuick 2.6
+import QtQuick 2.7
 import QtQuick.Controls 2.0
 import QtQuick.Layouts 1.3
 import "componentCreation.js" as MyScript
@@ -9,8 +9,6 @@ ApplicationWindow {
 	height: 600
 	visible: true
 	title: "SVG Editor"
-
-	// toolbar
 
 	header: ToolBar {
 		RowLayout {
@@ -35,9 +33,6 @@ ApplicationWindow {
 		Image {
 			source: "castle.png"
 		}
-
 	}
-
-
 }
 
